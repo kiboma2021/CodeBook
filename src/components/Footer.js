eed77b875c8354546dab4ef981c808b94b3e6a57
kiboma <omayioben@gmail.com>
2024-01-31 08:24:11 +0300

    Set darkmode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ export const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">CodeBook</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 sm:text-center dark:text-white">© 2023 <a href="/" className="hover:underline">CodeBook</a>. All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center">
+        <ul className="flex flex-wrap items-center dark:text-white">
             <li>
                 <a href="#" className="hover:underline me-4 md:me-6"><i className="fa-brands fa-instagram"></i></a>
             </li>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react'
 import { Link,NavLink } from 'react-router-dom'
 import Logo from '../assets/logo.jpeg'
-import { useEffect, useState } from 'react'
+
 
 export const Header = () => {
     const [darkMode, setDarkMode] = useState(true);
+    const [hiddenMode, setHiddenMode] = useState(true);
 
     useEffect(() => {
+        localStorage.setItem('darkMode',JSON.stringify(darkMode))
+        if (darkMode){
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
         
     },[darkMode])
+
+
 
 
   return (
@@ -17,44 +27,44 @@ export const Header = () => {
                 <img src={Logo} className="h-8" alt="Kibs Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kibs CodeBook</span>
             </Link>
-            <div className="flex items-center gap-3">
-                <div onClick={()=>setDarkMode()}>
-
+            <div className="flex items-center gap-3 dark:text-white">
+                <div onClick={()=>setDarkMode(!darkMode)}>
+                    <i className='fas fa-cog text-2xl' ></i>
                 </div>
-                <i className='fas fa-cog text-2xl' ></i>
+                
                 <NavLink to="/search"><i className='fas fa-search text-2xl' ></i> </NavLink>
                 <NavLink to="/cart"><i className='fas fa-shopping-cart text-2xl' ></i> </NavLink>
                 
                 
-                
-                
-
-                <div id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <div onClick={()=>setHiddenMode(!hiddenMode)} id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <i className='fas fa-user-circle text-2xl' ></i>
                 </div>
+                
+
+
+
+
+
+                
+            </div>
+        </div>
 
                 {/* Dropdown menu */}
-                <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-                    <div className="px-4 py-3">
-                    <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-                    </div>
-                    <ul className="py-2" aria-labelledby="user-menu-button">
-                    <li>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
+        <div className={`${hiddenMode?"hidden":""} flex justify-end z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} id="user-dropdown">
+            <ul className="py-2" aria-labelledby="user-menu-button">
+            <li>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+            </li>
+            <li>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+            </li>
+            <li>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+            </li>
+            <li>
+                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+            </li>
+            </ul>
         </div>
     </nav>
   )
