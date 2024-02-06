cd3039f9decc8918150f515b21ea5c9b624e7354
kiboma <omayioben@gmail.com>
2024-02-06 09:24:15 +0300

    Style drop down

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,26 +41,76 @@ export const Header = () => {
                 <div onClick={()=>setHiddenMode(!hiddenMode)} id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <i className='fas fa-user-circle text-2xl' ></i>
                 </div>
-                                
-            </div>
-        </div>
 
                 {/* Dropdown menu */}
-        <div className={`${hiddenMode?"hidden":""} flex justify-end z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600`} id="user-dropdown">
-            <ul className="py-2" aria-labelledby="user-menu-button">
-            <li>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-            </li>
-            </ul>
+                <div className={`${hiddenMode?"hidden":""} relative mt-10`}>
+            <div
+                className="absolute end-0 z-10 mt-2 w-56 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-lg"
+                role="menu"
+            >
+                <div className="p-2">
+                <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                    role="menuitem"
+                >
+                    View on Storefront
+                </a>
+
+                <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                    role="menuitem"
+                >
+                    View Warehouse Info
+                </a>
+
+                <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                    role="menuitem"
+                >
+                    Duplicate Product
+                </a>
+
+                <a
+                    href="#"
+                    className="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                    role="menuitem"
+                >
+                    Unpublish Product
+                </a>
+                </div>
+
+                <div className="p-2">
+                <form method="POST" action="#">
+                    <button
+                    type="submit"
+                    className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-700 hover:bg-red-50"
+                    role="menuitem"
+                    >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                    >
+                        <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                    </svg>
+
+                    Delete Product
+                    </button>
+                </form>
+                </div>
+            </div>
+                </div>              
+            </div>
         </div>
     </nav>
   )
