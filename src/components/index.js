4a1997aa71dd205fdfcd646e76c2d2c282537389
kiboma <omayioben@gmail.com>
2024-02-19 15:02:58 +0300

    Export authentication components

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,8 @@ export { Banner } from "../pages/Home/Banner";
 export { StudentReview } from "../pages/Home/StudentReview";
 export { CartCard } from "./CartCard";
 export { SearchCard } from "./SearchCard";
+export { AuthenticatedUser } from "./AuthenticatedUser";
+export { UnAuthenticatedUser } from "./UnAuthenticatedUser";
 
 
 
